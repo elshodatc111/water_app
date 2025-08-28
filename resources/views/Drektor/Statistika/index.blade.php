e7dce30efe4b5325d9df3caba1ed8deb15232950
Elshod Musurmonov <elshodatc1116@gmail.com>
2025-08-28 23:24:45 +0500

    Save

--- resources/views/Drektor/Statistika/index.blade.php
+++ resources/views/Drektor/Statistika/index.blade.php
@@ -15,9 +15,162 @@
             {{ session('success') }}
         </div>
     @endif
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Kunlik buyurtmalar</h3>
+                    <div id="kunlikBuyurtma"></div>
+                    <script>
+                        document.addEventListener("DOMContentLoaded", () => {
+                        new ApexCharts(document.querySelector("#kunlikBuyurtma"), {
+                                series: [44, 55, 13,],
+                                chart: {
+                                    height: 350,
+                                    type: 'pie',
+                                    toolbar: {show: true}
+                                },
+                                labels: ['Yangi buyurtmalar', 'Yetqazilgan buyurtmalar', 'Yakunlangan buyurtmalar']
+                            }).render();
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Oylik buyurtmalar</h3>
+                    <div id="oylikBuyurtma"></div>
+                    <script>
+                        document.addEventListener("DOMContentLoaded", () => {
+                        new ApexCharts(document.querySelector("#oylikBuyurtma"), {
+                                series: [44, 55, 13,],
+                                chart: {
+                                    height: 350,
+                                    type: 'pie',
+                                    toolbar: {show: true}
+                                },
+                                labels: ['Yangi buyurtmalar', 'Yetqazilgan buyurtmalar', 'Yakunlangan buyurtmalar']
+                            }).render();
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="card">
         <div class="card-body">
-            <h3 class="card-title">Statistika</h3>
+            <h3 class="card-title">Kunlik Statistika</h3>
+            <div id="kunlikBuyurtma2"></div>
+              <script>
+                document.addEventListener("DOMContentLoaded", () => {
+                    new ApexCharts(document.querySelector("#kunlikBuyurtma2"), {
+                        series: [{
+                        name: 'Yetqazildi',
+                        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+                        }, {
+                        name: 'Bekor qilingan',
+                        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+                        }],
+                        chart: {
+                        type: 'bar',
+                        height: 350
+                        },
+                        plotOptions: {
+                        bar: {
+                            horizontal: false,
+                            columnWidth: '55%',
+                            endingShape: 'rounded'
+                        },
+                        },
+                        colors: ['#28a745', '#dc3545'], // 1-seriya (Yetqazildi) yashil, 2-seriya (Bekor qilingan) qizil
+                        dataLabels: {
+                        enabled: false
+                        },
+                        stroke: {
+                        show: true,
+                        width: 2,
+                        colors: ['transparent']
+                        },
+                        xaxis: {
+                        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+                        },
+                        yaxis: {
+                        title: {
+                            text: 'Oylik buyurtmalar'
+                        }
+                        },
+                        fill: {
+                        opacity: 1
+                        },
+                        tooltip: {
+                        y: {
+                            formatter: function(val) {
+                            return val + " ta"
+                            }
+                        }
+                        }
+                    }).render();
+                });
+              </script>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">Oylik Statistika</h3>
+            <div id="oylikStatistika"></div>
+              <script>
+                document.addEventListener("DOMContentLoaded", () => {
+                    new ApexCharts(document.querySelector("#oylikStatistika"), {
+                        series: [{
+                        name: 'Yetqazildi',
+                        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+                        }, {
+                        name: 'Bekor qilingan',
+                        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+                        }],
+                        chart: {
+                        type: 'bar',
+                        height: 350
+                        },
+                        plotOptions: {
+                        bar: {
+                            horizontal: false,
+                            columnWidth: '55%',
+                            endingShape: 'rounded'
+                        },
+                        },
+                        colors: ['#28a745', '#dc3545'], // 1-seriya (Yetqazildi) yashil, 2-seriya (Bekor qilingan) qizil
+                        dataLabels: {
+                        enabled: false
+                        },
+                        stroke: {
+                        show: true,
+                        width: 2,
+                        colors: ['transparent']
+                        },
+                        xaxis: {
+                        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+                        },
+                        yaxis: {
+                        title: {
+                            text: 'Oylik buyurtmalar'
+                        }
+                        },
+                        fill: {
+                        opacity: 1
+                        },
+                        tooltip: {
+                        y: {
+                            formatter: function(val) {
+                            return val + " ta"
+                            }
+                        }
+                        }
+                    }).render();
+                });
+              </script>
         </div>
     </div>
 @endsection
